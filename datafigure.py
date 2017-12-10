2a8c7ec1af2d4a48f8c2b1cdf510c2a8669da890
369696807 <369696807@qq.com>
2017-12-10 09:20:53 +0800

    pandas plotting

---
+++ datafigure.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import matplotlib.pyplot as plt 
+# def main():
+try:
+    with open('user_study.json') as js:
+        _df = pd.read_json(js)
+        df = _df[['user_id', 'minutes']].groupby('user_id').sum()
+
+        ax = df.plot()
+        ax.set_xlabel('User ID')
+        ax.set_ylabel('Study Time')
+        ax.set_title('Study Data')
+
+        plt.show()
+except:
+    pass
+
+
